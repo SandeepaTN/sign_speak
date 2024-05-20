2d6f44a73e30d4e60b769b2a39d6aa546994e57b
sandeepa t n <saandeepatalagini@gmail.com>
2024-05-20 16:23:44 +0530

    first commit

---
+++ app.py
@@ -0,0 +1,98 @@
+
+import cv2
+import numpy as np
+import mediapipe as mp
+from tensorflow.keras.models import load_model
+import gradio as gr
+from concurrent.futures import ThreadPoolExecutor
+import pyttsx3
+
+
+# Load the sign language recognition model
+model = load_model('isl.h5')
+
+# Initialize Mediapipe
+mp_holistic = mp.solutions.holistic
+mp_drawing = mp.solutions.drawing_utils
+
+
+# Define actions
+actions = ['hello', 'help' ,'home', 'no', 'phone', 'thank you', 'there' ,'victory', 'water','yes']
+ 
+# Function to perform Mediapipe detection
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+    image.flags.writeable = False
+    results = model.process(image)
+    image.flags.writeable = True
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR)
+    return image, results
+
+# Function to extract keypoints
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([pose, lh, rh])
+
+
+
+    
+# Create a ThreadPoolExecutor with one thread
+executor = ThreadPoolExecutor(max_workers=1)
+
+def speak_async(text):
+    engine = pyttsx3.init()  # Create a new instance of the speech synthesis engine
+    engine.say(text)
+    engine.runAndWait()
+
+def speak(text):
+    # Submit the speak_async function to the ThreadPoolExecutor
+    executor.submit(speak_async, text)
+
+# Function to predict sign from video
+def predict_sign_from_video(video_path):
+    cap = cv2.VideoCapture(video_path)
+    frames = []
+    with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+        while cap.isOpened():
+            ret, frame = cap.read()
+            if not ret:
+                break
+            
+            image, results = mediapipe_detection(frame, holistic)
+            keypoints = extract_keypoints(results)
+            frames.append(keypoints)
+            if len(frames) == 30:
+                sequence = np.array(frames)
+                res = model.predict(np.expand_dims(sequence, axis=0))[0]
+                sign = actions[np.argmax(res)]
+                # Speak the predicted sign
+                speak(sign)
+                frames = []  # Reset frames for next sequence
+                return sign
+    
+    cap.release()
+    
+
+    
+# Define examples
+
+examples = [
+        ['videos/qwe.mp4'],
+        ['videos/asd.mp4']
+        
+    ]
+
+    
+    
+# Create Gradio Interface
+iface = gr.Interface(predict_sign_from_video,
+                     inputs="video",
+                     outputs="text", 
+                     title="Sign Speak",
+                     description="Upload a video and get the predicted sign spoken and displayed.",
+                     examples=examples,
+                     cache_examples=False)
+iface.launch(share=True)
+
---
+++ run.py
@@ -0,0 +1,124 @@
+#%pip install tensorflow  opencv-python mediapipe sklearn-pandas matplotlib seaborn gradio
+import cv2
+import numpy as np
+import mediapipe as mp
+from tensorflow import keras
+
+
+cv2.namedWindow('Sign Speak', cv2.WINDOW_NORMAL)
+
+
+mp_holistic = mp.solutions.holistic # Holistic model
+mp_drawing = mp.solutions.drawing_utils # Drawing utilities
+actions=np.array(['hello', 'help' ,'home', 'no', 'phone', 'thank you', 'there' ,'victory', 'water','yes'])
+
+def mediapipe_detection(image, model):
+    image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) # COLOR CONVERSION BGR 2 RGB
+    image.flags.writeable = False                  # Image is no longer writeable
+    results = model.process(image)                 # Make prediction
+    image.flags.writeable = True                   # Image is now writeable 
+    image = cv2.cvtColor(image, cv2.COLOR_RGB2BGR) # COLOR COVERSION RGB 2 BGR
+    return image, results
+
+
+def draw_landmarks(image, results):
+    
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS) # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS) # Draw right hand connections
+
+
+def draw_styled_landmarks(image, results):
+    
+    # Draw pose connections
+    mp_drawing.draw_landmarks(image, results.pose_landmarks, mp_holistic.POSE_CONNECTIONS,
+                             mp_drawing.DrawingSpec(color=(80,22,10), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(80,44,121), thickness=2, circle_radius=2)
+                             ) 
+    # Draw left hand connections
+    mp_drawing.draw_landmarks(image, results.left_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                             mp_drawing.DrawingSpec(color=(121,22,76), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(121,44,250), thickness=2, circle_radius=2)
+                             ) 
+    # Draw right hand connections  
+    mp_drawing.draw_landmarks(image, results.right_hand_landmarks, mp_holistic.HAND_CONNECTIONS, 
+                             mp_drawing.DrawingSpec(color=(245,117,66), thickness=2, circle_radius=4), 
+                             mp_drawing.DrawingSpec(color=(245,66,230), thickness=2, circle_radius=2)
+                             )   
+
+
+def extract_keypoints(results):
+    pose = np.array([[res.x, res.y, res.z, res.visibility] for res in results.pose_landmarks.landmark]).flatten() if results.pose_landmarks else np.zeros(33*4)
+    
+    lh = np.array([[res.x, res.y, res.z] for res in results.left_hand_landmarks.landmark]).flatten() if results.left_hand_landmarks else np.zeros(21*3)
+    rh = np.array([[res.x, res.y, res.z] for res in results.right_hand_landmarks.landmark]).flatten() if results.right_hand_landmarks else np.zeros(21*3)
+    return np.concatenate([pose,  lh, rh])
+
+
+model=keras.models.load_model('_internal/isl.h5')
+
+
+
+
+
+# 1. New detection variables
+sequence = []
+sentence = []
+predictions = []
+threshold = 0.7
+
+cap = cv2.VideoCapture(0)
+# Set mediapipe model 
+with mp_holistic.Holistic(min_detection_confidence=0.5, min_tracking_confidence=0.5) as holistic:
+    while cap.isOpened():
+
+        # Read feed
+        ret, frame = cap.read()
+
+        # Make detections
+        image, results = mediapipe_detection(frame, holistic)
+        # print(results)
+        
+        # Draw landmarks
+        draw_styled_landmarks(image, results)
+        
+        # 2. Prediction logic
+        keypoints = extract_keypoints(results)
+        sequence.append(keypoints)
+        sequence = sequence[-30:]
+        
+        if len(sequence) == 30:
+            res = model.predict(np.expand_dims(sequence, axis=0))[0]
+            
+            predictions.append(np.argmax(res))
+            
+            
+        #3. Viz logic
+            if np.unique(predictions[-10:])[0]==np.argmax(res): 
+                if res[np.argmax(res)] > threshold: 
+                    
+                    if len(sentence) > 0: 
+                        if actions[np.argmax(res)] != sentence[-1]:
+                            sentence.append(actions[np.argmax(res)])
+                    else:
+                        sentence.append(actions[np.argmax(res)])
+
+            if len(sentence) > 5: 
+                sentence = sentence[-5:]
+
+           
+            
+        cv2.rectangle(image, (0,0), (640, 40), (245, 117, 16), -1)
+        cv2.putText(image, ' '.join(sentence), (3,30), 
+                       cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 255), 2, cv2.LINE_AA)
+        
+        # Show to screen
+        cv2.imshow('Sign Speak', image)
+
+        # Break gracefully
+        
+        if cv2.waitKey(10) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+
